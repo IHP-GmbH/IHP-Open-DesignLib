1fc013703aad369311fff677867716e4acec68d5
Daniel Schultz <d.schultz@phytec.de>
2024-10-22 09:20:35 +0200

    Allocate i2c-gpio-expander Design
    Add the project structure and source files for an i2c-gpio-expander
    design. This design should be part of run T576.
    
    Signed-off-by: Daniel Schultz <d.schultz@phytec.de>

---
+++ i2c-gpio-expander/README.rst
@@ -0,0 +1,25 @@
+Open Source I2C Gpio Expander
+=============================
+
+This directory contains the design files for an 8-bit I2C GPIO expander, developed using IHP's SG13G2 process. This design is part of the MPW run T576. The expander allows communication via I2C to control 8 GPIO pins, suitable for various interfacing and expansion purposes.
+
+Files Included
+##############
+
+* RTL source code
+* Layout files (TBD)
+* Simulation and verification scripts (TBD)
+* Documentation (TBD)
+
+More Information
+################
+
+For additional details, design rationale, and contributions, please check out the original repository at `i2c-gpio-expander`_.
+
+License
+#######
+
+This design is open source and available under the `GPLv3 license`_.
+
+.. _GPLv3 license: COPYING.GPLv3
+.. _i2c-gpio-expander: https://github.com/aesc-silicon/i2c-gpio-expander
---
+++ i2c-gpio-expander/design_data/src/SG13G2Top.mk
@@ -0,0 +1,18 @@
+export DESIGN_NAME=SG13G2Top
+export PLATFORM=ihp-sg13g2
+
+export VERILOG_FILES= /home/daniel/work/aesc/i2c-gpio-expander/modules/elements/zibal//build//I2cGpioExpander/SG13G2/zibal/SG13G2Top.v
+export SDC_FILE=/home/daniel/work/aesc/i2c-gpio-expander/modules/elements/zibal//build//I2cGpioExpander/SG13G2/zibal/SG13G2Top.sdc
+
+export SEAL_GDS = /home/daniel/work/aesc/i2c-gpio-expander/modules/elements/zibal//build//I2cGpioExpander/SG13G2/zibal//macros/sealring/sealring.gds.gz
+
+export DIE_AREA = 0.0 0.0 1050.0 1050.0
+export CORE_AREA = 425.28 427.16 631.2 630.24
+
+export MAX_ROUTING_LAYER = TopMetal2
+export HAS_IO_RING = 1
+
+export TNS_END_PERCENT = 100
+
+export FOOTPRINT_TCL = /home/daniel/work/aesc/i2c-gpio-expander/modules/elements/zibal//build//I2cGpioExpander/SG13G2/zibal/SG13G2Top.pad.tcl
+export PDN_TCL = /home/daniel/work/aesc/i2c-gpio-expander/modules/elements/zibal//build//I2cGpioExpander/SG13G2/zibal/SG13G2Top.pdn.tcl
